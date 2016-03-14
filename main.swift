13a620630d697d70de426044692abde7e87ba144
luckysiqin <a20131105804@Mac28.local>
2016-03-14 20:57:47 +0800

    canshu
    多种多样的函数参数

---
+++ main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  20131105804-definition
+//
+//  Created by 20131105804 on 16/3/10.
+//  Copyright © 2016年 20131105804. All rights reserved.
+//
+
+import Foundation
+
+func multipleOfTen(multiplier:Int){
+   print("\(multiplier) * 10 =  \(multiplier * 10)")
+
+}
+multipleOfTen(10)
+func multiple0f(multiplier: Int,andValue: Int){
+   print("\(multiplier) * \(andValue) = \(multiplier * andValue)")
+}
+multiple0f(5, andValue: 10)
